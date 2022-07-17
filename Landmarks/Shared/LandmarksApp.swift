a0942e4008d1e5a0293b5e7a80c8643cd2f4ba2d
zhaolong <superzhaolong@126.com>
2022-07-17 17:11:13 +0800

    Creating and Combining Views

---
+++ Landmarks/Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by 赵龙 on 2022/7/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
